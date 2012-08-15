90e0be4b3f4a6438d593ab493520a5baea24eacb
Marty Hines <mhines76@gmail.com>
2012-08-15 14:52:40 -0400

    change folder structure

---
+++ php/proxy.php
@@ -0,0 +1,5 @@
+<?php
+//header('Content-type: application/javascript; charset: utf-8');
+$url = "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=1b0bdb3eaa5806a5a1f4d10a50eed28d:6:66427984";
+echo file_get_contents($url);
+?>
